85ccfc5e2c2c95dcb944a9053c7a65c059f6b59b
JMukuni <bossjefter93@gmail.com>
2021-09-28 18:55:33 -0700

    Code fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-Square = __import__('1-square').Square
+class Square:
+    """Represents a square.
+    Private instance attribute: size.
+    Instantiation with size (no type/value verification).
+    """
 
-my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
-
-try:
-    print(my_square.size)
-except Exception as e:
-    print(e)
-try:
-    print(my_square.__size)
-except Exception as e:
-    print(e)
+    def __init__(self, size):
+        """Initializes the data."""
+        self.__size = size
